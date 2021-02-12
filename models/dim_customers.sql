0935520a01b51d6ec72d085da361ac543291d1f5
Sachin Kumar <sachin.kumar@underarmour.com>
2021-02-12 16:22:21 +0000

    stg tables move to model folder

--- models/dim_customers.sql
+++ models/dim_customers.sql
@@ -1,18 +1,28 @@
-{{
-    config(materialized="table")
-}}
-
 with customers as (
 
-    select * 
-    from {{ref("stg_customers")}}
+    select * from {{ ref('stg_customers')}}
 
 ),
 
 orders as (
 
-    select *        
-    from {{ref("stg_orders")}}
+    select * from {{ ref('stg_orders') }}
+
+),
+
+customer_orders as (
+
+    select
+        customer_id,
+
+        min(order_date) as first_order_date,
+        max(order_date) as most_recent_order_date,
+        count(order_id) as number_of_orders
+
+    from orders
+
+    group by 1
+
 ),
 
 
